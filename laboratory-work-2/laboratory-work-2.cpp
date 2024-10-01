f825bf3fc948ea81c0d5ff0a0bcb9ac6e594e1d1
Danil Domnich <domni4.d2@yandex.ru>
2024-10-01 12:40:12 +0300

    Обновлен файл лабораторной работы и добавлен исполняемый файл

--- laboratory-work-2/laboratory-work-2.cpp
+++ laboratory-work-2/laboratory-work-2.cpp
@@ -1,1 +1,38 @@
-s
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+unsigned long long int factorial(int number)
+{
+    if (number > 1) {
+        return factorial(number - 1) * number;
+    }
+    else {
+        return number;
+    }
+}
+
+long double func(int n)
+{
+    long double result = 1;
+    for (int k = 1; k <= n; k++) {
+        result += pow(-1, k) / (factorial(2 * k));
+    }
+    return result;
+}
+
+int main()
+{
+    int counter = 2;
+    setlocale(LC_ALL, "Russian");
+    for (int n = 1; n < 10; n++) {
+        if ((fabs(cos(1) - func(n)) < pow(10, -counter)) && (counter <= 6)) {
+            cout << "Для точности 10^" << -counter << " требуется " << n << " ш." << endl;
+            cout << fixed << setprecision(6) << func(n) << endl;
+            counter++;
+        }
+    }
+    return 0;
+}
